e2f95b3ea72cf8e7387021fd7126958f706c5b07
Arisekola <shuaibazeez14@gmail.com>
2024-04-28 12:11:58 +0100

    download removed

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -8,15 +8,7 @@ import { fadeIn, slideIn, staggerContainer, textVariant, textVariant2 } from "..
 
 const About = ({darkmode}) => {
 
-  const handleDownload = () => {
-
-    const link = document.createElement('a');
-    link.href = akinwale; 
-    link.download = 'akinwale.jpg'; 
-    document.body.appendChild(link); 
-    link.click(); 
-    document.body.removeChild(link); 
-  }
+ 
   return (
     <motion.div 
     variants={staggerContainer}
@@ -56,9 +48,7 @@ const About = ({darkmode}) => {
                     <FaEnvelope />
                     <p>Bakare.john2015@gmail.com</p>
                     </div>
-              <button onClick={handleDownload} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
-                Download Image
-              </button>
+                 
                 </motion.div>
              </motion.div>
              <div className="flex flex-col justify-center items-center">
